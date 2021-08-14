02709198d8bf2d12b67d634fa0a2fdec1101660f
Sean Gregory <sean.christopher.gregory@gmail.com>
2021-08-14 09:07:54 -0400

    add thing

--- lib/graphlyte/types.rb
+++ lib/graphlyte/types.rb
@@ -1,30 +1,9 @@
 require_relative "schema/types/base"
-require_relative "schema/types/enum"
-require_relative "schema/types/input_object"
-require_relative "schema/types/interface"
-require_relative "schema/types/list"
-require_relative "schema/types/non_null"
-require_relative "schema/types/object"
-require_relative "schema/types/scalar"
-require_relative "schema/types/union"
-
 
 module Graphlyte
   class Types
     def method_missing(method, placeholder)
       Schema::Types::Base.new(method, placeholder)
-    end
-
-    def ID(placeholder)
-      Schema::Types::Scalar.new("ID", placeholder)
-    end
-    
-    def ID!(placeholder)
-      Schema::Types::Scalar.new("ID!", placeholder)
-    end
-
-    def INPUT_OBJECT(type, placeholder)
-      Schema::Types::InputObject.new(type, placeholder)
     end
   end
 end
